3e5ad25f0e9949cf320abe72360248b019e2b378
hhowaitorozu <nrinsalwa@gmail.com>
2026-05-07 14:13:14 +0700

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
+  base: "/portfolio-rin/", 
+  plugins: [react(), tailwindcss()],
 });
